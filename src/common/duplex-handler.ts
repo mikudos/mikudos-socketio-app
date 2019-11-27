9e3912263c5467336799388a58e3c68c07ca703f
guanyu <yue.guanyu@hotmail.com>
2019-11-28 00:17:19 +0800

    change to use self managed eventEmitter

--- src/common/duplex-handler.ts
+++ src/common/duplex-handler.ts
@@ -1,9 +1,11 @@
 import { get, set, unset } from 'lodash';
+import socket from 'socket.io';
 import { HandlerBase } from './handler-base';
+import { EventEmitter } from 'events';
 
 export class DUPLEX_HANDLER extends HandlerBase {
     namespaces: any = {};
-    socketStreams: any = {};
+    socketStreams: { [key: string]: EventEmitter } = {};
     constructor(namespaces: object, { eventPath = 'stream-call' } = {}) {
         super(eventPath);
         this.namespaces = namespaces;
@@ -13,14 +15,24 @@ export class DUPLEX_HANDLER extends HandlerBase {
         namespace: string,
         method: string,
         data: any,
-        socket: SocketIO.Socket
+        socket: socket.Socket
     ) {
+        let event = get(this.socketStreams, socket.id);
+        if (!event || !(event instanceof EventEmitter)) {
+            event = new EventEmitter();
+            set(this.socketStreams, socket.id, event);
+        }
         try {
-            let res = this.namespaces[namespace].handle(method, data);
-            set(this.socketStreams, `${socket.id}.${namespace}.${method}`, res);
-            this.socketStreams[socket.id][namespace][method].on(
-                'data',
+            this.namespaces[namespace].handle(namespace, method, data, event);
+            console.log(
+                'TCL: this.socketStreams',
+                socket.id,
+                this.socketStreams
+            );
+            this.socketStreams[socket.id].on(
+                `${namespace}.${method}`,
                 (data: any) => {
+                    console.log('TCL: socketStreams data', data);
                     socket.emit(
                         `${this.eventPath} ${namespace}.${method}`,
                         data
@@ -28,12 +40,8 @@ export class DUPLEX_HANDLER extends HandlerBase {
                 }
             );
         } catch (error) {
-            let res = get(
-                this.socketStreams,
-                `${socket.id}.${namespace}.${method}`
-            );
-            if (res) res.removeAllListeners();
-            unset(this.socketStreams, `${socket.id}.${namespace}.${method}`);
+            let event = get(this.socketStreams, socket.id);
+            if (event) event.removeAllListeners(`${namespace}.${method}`);
             return { error: { message: 'Request Error' } };
         }
         return { result: { successed: true } };
@@ -45,54 +53,44 @@ export class DUPLEX_HANDLER extends HandlerBase {
         data: any,
         socket: SocketIO.Socket
     ) {
-        let res = get(
-            this.socketStreams,
-            `${socket.id}.${namespace}.${method}`
-        );
-        if (!res)
+        let event = get(this.socketStreams, socket.id);
+        if (!event)
             return { error: { message: 'Request Not exist or is finished' } };
-        try {
-            res.send(data);
-        } catch (error) {
-            return {
-                error: {
-                    message:
-                        error.message || 'Send Error, Request may be closed'
-                }
-            };
-        }
-        return { result: { successed: true } };
+        event.emit(`${namespace}.${method} send`, data);
+        return await new Promise((resolve, reject) => {
+            event.once(`${namespace}.${method} send`, data => resolve(data));
+            setTimeout(() => {
+                resolve({
+                    error: { message: 'Request Timeout' }
+                });
+            }, 10000);
+        });
     }
 
     async cancel(namespace: string, method: string, socket: SocketIO.Socket) {
-        let res = get(
-            this.socketStreams,
-            `${socket.id}.${namespace}.${method}`
-        );
-        if (!res)
+        let event = get(this.socketStreams, socket.id);
+        if (!event)
             return {
                 error: {
                     message: 'Cancel Error, Request may be closed'
                 }
             };
         // cancel first
-        res.removeAllListeners();
-        unset(this.socketStreams, `${socket.id}.${namespace}.${method}`);
-        return { result: { successed: true } };
+        event.emit(`${namespace}.${method} cancel`);
+        event.removeAllListeners(`${namespace}.${method}`);
+        return await new Promise((resolve, reject) => {
+            event.once(`${namespace}.${method} cancel`, data => resolve(data));
+            setTimeout(() => {
+                resolve({
+                    error: { message: 'Request Timeout' }
+                });
+            }, 10000);
+        });
     }
 
     cancelAllOnSocket(id: string) {
-        for (const key in this.socketStreams[id]) {
-            if (this.socketStreams[id].hasOwnProperty(key)) {
-                const namespace = this.socketStreams[id][key];
-                for (const nk in namespace) {
-                    if (namespace.hasOwnProperty(nk)) {
-                        const res = namespace[nk];
-                        if (res) res.removeAllListeners();
-                    }
-                }
-            }
-        }
+        let event = this.socketStreams[id];
+        event.removeAllListeners();
         unset(this.socketStreams, id);
         return { result: { successed: true } };
     }
