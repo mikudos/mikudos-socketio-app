953007f64a645e7c7bfec24b9f9d2644b958c700
guanyu <yue.guanyu@hotmail.com>
2019-11-27 17:52:39 +0800

    add export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { Application } from './app';
+export { Authentication, AuthenticationRequest } from './authentication.class';
 export { ServiceMethods, JSON_RPC_HANDLER, ChatHandler } from './common';
