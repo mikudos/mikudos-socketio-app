35ddcecfc74eb001cf6f94f3b87c983f58f68179
guanyu <yue.guanyu@hotmail.com>
2019-12-08 21:11:59 +0800

    switch to transport the app

--- src/app.ts
+++ src/app.ts
@@ -134,7 +134,7 @@ export class Application {
                                 this.authentication.authJoinCallback &&
                                 this.authentication.authJoinCallback(
                                     socket,
-                                    this.io
+                                    this
                                 );
                         });
                     }
--- src/authentication.class.ts
+++ src/authentication.class.ts
@@ -17,10 +17,7 @@ export class Authentication {
     };
     eventPath: string;
     tokenPath: string;
-    authJoinCallback: (
-        socket: SocketIO.Socket,
-        io?: SocketIO.Server | SocketIO.Namespace
-    ) => void;
+    authJoinCallback: (socket: SocketIO.Socket, app?: Application) => void;
     constructor(
         {
             protocol = 'http',
@@ -35,7 +32,7 @@ export class Authentication {
             eventPath = 'authentication',
             authJoinCallback = async (
                 socket: SocketIO.Socket,
-                io?: SocketIO.Server | SocketIO.Namespace
+                app?: Application
             ) => {}
         } = {}
     ) {
