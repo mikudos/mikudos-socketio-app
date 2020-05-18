713ba17c6e583dd1400647e05b12114495fab92c
guanyu <yue.guanyu@hotmail.com>
2020-05-18 12:21:03 +0800

    update version

--- src/namespace.ts
+++ src/namespace.ts
@@ -2,41 +2,41 @@ import { Application } from './app';
 import { DUPLEX_HANDLER } from './common';
 
 export declare namespace mikudos {
-    interface duplexHooks {
-        [key: string]: ((
-            eventName: string,
-            data: any,
-            socket: mikudos.Socket
-        ) => Promise<any>)[];
-    }
-    interface ConfigFunc {
-        (app: Application): void;
-    }
+  interface duplexHooks {
+    [key: string]: ((
+      eventName: string,
+      data: any,
+      socket: mikudos.Socket,
+    ) => Promise<any>)[];
+  }
+  interface ConfigFunc {
+    (app: Application): void;
+  }
 
-    interface Socket extends SocketIO.Socket {
-        mikudos: {
-            app: Application;
-            provider: string;
-            headers: any;
-            remoteAddress: any;
-            user: any;
-        };
-    }
+  interface Socket extends SocketIO.Socket {
+    mikudos: {
+      app: Application;
+      provider: string;
+      headers: any;
+      remoteAddress: any;
+      user: any;
+    };
+  }
 
-    interface DuplexService {
-        serviceKey: string;
-        serviceClass: any;
-        before: { [key: string]: DuplexHandle[] };
-        [key: string]: any;
-    }
+  interface DuplexService {
+    serviceClass: any;
+    before: { [key: string]: DuplexHandle[] };
+    [key: string]: any;
+  }
 
-    interface DuplexServiceConstructor {
-        new (handler: DUPLEX_HANDLER, app: Application): DuplexService;
-    }
+  interface DuplexServiceConstructor {
+    serviceKey: string;
+    new (handler: DUPLEX_HANDLER, app: Application): DuplexService;
+  }
 
-    interface DuplexHandle {
-        (eventName: string, data: any, socket: mikudos.Socket): Promise<
-            void
-        > | void;
-    }
+  interface DuplexHandle {
+    (eventName: string, data: any, socket: mikudos.Socket): Promise<
+      void
+    > | void;
+  }
 }
